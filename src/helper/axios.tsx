37289552c2f3971474440659dcd9edd964321a48
vinaykumar231 <vinaymahto999@gmail.com>
2024-11-13 19:16:18 +0530

    add

--- src/helper/axios.tsx
+++ src/helper/axios.tsx
@@ -3,11 +3,11 @@ import axios from "axios";
 export default axios.create({
   // baseURL: "https://il8rigour.com/",
   // baseURL: "http://il8rigour.com:8000/",
-  baseURL: "https://ai.neovis.co.in/interview/",
+  baseURL: "https://3fbb-2405-201-37-21d9-399e-800d-e79c-9d90.ngrok-free.app/",
   // baseURL: "https://lms-5wr7.onrender.com/",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
-   
+    "ngrok-skip-browser-warning": true,
   },
 });
